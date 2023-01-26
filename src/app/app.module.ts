5e2839420e2276b55accd21356dc25e2cce14343
basganan <bas.ganan@yavirac.edu.ec>
2023-01-26 00:12:09 -0500

    arreglos app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { ProductComponent } from './pages/product/product.component';
-import { CatalogueComponent } from './pages/catalogue/catalogue.component';
-import { PagesComponent } from './pages/pages.component';
-import { PagesModule } from './pages/pages.module';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './auth/auth.module';
-import { AuthComponent } from './auth/auth.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PagesComponent } from './pages/pages.component';
+import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PageModule } from './pages/page.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    //DashboardComponent,
+    PagesComponent,
     NopagesfoundComponent,
-    SidebarComponent,
-    HeaderComponent,
-    FooterComponent,
-    BreadcrumbsComponent,
-    //AuthComponent,
-    //ProductComponent,
-    //CatalogueComponent,
-    //PagesComponent
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule,
+    PageModule,
     SharedModule,
-    AuthModule,
-    HttpClientModule
-
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
